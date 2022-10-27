b6317dbca6ecc4ac4b71144c7efa6f6aa14c70d1
andresOlarte <deuterocanonico@gmail.com>
2022-10-27 06:58:20 -0700

    first commit

---
+++ java/hamcrest/src/main/java/hamcrest/cristmaslights/ChristmasLights.java
@@ -0,0 +1,5 @@
+package hamcrest.cristmaslights;
+
+public class ChristmasLights {
+
+}
---
+++ java/hamcrest/src/test/java/hamcrest/cristmaslights/ChristmasLightsTest.java
@@ -0,0 +1,13 @@
+package hamcrest.cristmaslights;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class ChristmasLightsTest {
+
+	@Test
+	void canCreateChristmasLights() throws Exception {
+		ChristmasLights cLights = new ChristmasLights();
+	}
+}
